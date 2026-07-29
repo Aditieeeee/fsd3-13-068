65ceca58d4ec8cebbedfded04bd3aa956cca4d26
Aditieeeee <aditiguptareco@gmail.com>
2026-07-29 12:05:42 +0530

    delete functionality edit

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,4 +1,4 @@
-import{readFile,writeFile} from "fs/promises";
+import{readFile,writeFile,appendFile, unlink} from "fs/promises";
 
 const writeData = async (fname , contents)=>{
  await writeFile(fname,contents);
@@ -13,10 +13,16 @@ const readData = async (fname)=>{
 
 const appendData = async (fname , contents)=>{
     await appendFile(fname,"\n"+contents);
+    console.log('${fname} deleted')
 };
 
-await writeData("sad.txt","i am saddddddd..");
-await readData("sad.txt");
-await appendData("sad.txt","i am happy nowwwwwwwwwwwwwwwwwwwwwwwwwwww"); 
-// for append it midify the file content and add new content at the end of the file
-await readData("sad.txt");
+const deleteFile= async (fname)=>{
+    fstat.unlink(fname);
+}
+
+// await writeData("sad.txt","i am saddddddd..");
+// await readData("sad.txt");
+// await appendData("sad.txt","i am happy nowwwwwwwwwwwwwwwwwwwwwwwwwwww"); 
+// // for append it midify the file content and add new content at the end of the file
+// await readData("sad.txt");
+await deleteFile("sad.txt")

b610e103e1d3e5994255b0cdd6209bd804f1f315
Aditieeeee <aditiguptareco@gmail.com>
2026-09-16 11:46:38 +0530

    prg6 server and post get delete

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,14 +1,27 @@
 import  http from 'http';
 
 const server = http.createServer((req, res) => {
-   if(req.url ==="/api/products"){
-    res.statusCode = 200;
-    res.end("sent with 200");
+   if(req==="/" && req.method==="GET"){
+    res.statusCode=200;
+    res.end("get request received\n");
    }
-   else{
-    res.statusCode = 404;
-    res.end("not found");
+   else if(req.url==="/about" && req.method==="POST"){
+      res.statusCode=200;
+      res.end("post request received\n");
    }
-});
 
-server.listen(3000, () => console.log('prg6 is running'))
+   else if(req.url==="/about" && req.method==="PUT"){
+      res.statusCode=200;
+      res.end("put request received\n");
+   }
+
+   else if(req.url==="/about" && req.method==="DELETE"){
+      res.statusCode=200;
+      res.end("delete request received\n");
+   }
+
+   else{
+      res.statusCode=404;
+      res.end("Not Found\n");
+   }
+   

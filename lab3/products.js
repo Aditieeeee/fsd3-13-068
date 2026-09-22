5c4013286b8d8c1f3734c53d71d1029f69c609b6
Aditieeeee <aditiguptareco@gmail.com>
2026-09-22 14:17:17 +0530

    get allproducts api

---
+++ lab3/products.js
@@ -0,0 +1,16 @@
+const products =[
+    {id :1 , name:"laptop" , qty: 10, price: 50000},
+    {id :2 , name:"phone" , qty: 20, price: 20000}
+
+]
+
+let nextId =3;
+
+export const getAllProducts = () => {
+    return products;
+}
+export const addProducts = () =>{
+
+}
+
+// export { getAllProducts };

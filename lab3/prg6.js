8a83759ad4c153342e894b013610c8752227bd2e
Aditieeeee <aditiguptareco@gmail.com>
2026-09-22 13:27:56 +0530

    modified lab6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,27 +1,33 @@
-import  http from 'http';
+import http from "http";
 
 const server = http.createServer((req, res) => {
-   if(req==="/" && req.method==="GET"){
-    res.statusCode=200;
-    res.end("get request received\n");
-   }
-   else if(req.url==="/about" && req.method==="POST"){
-      res.statusCode=200;
-      res.end("post request received\n");
-   }
+  if (req.url === "/" && req.method === "GET") {
+    res.statusCode = 200;
+    res.end("GET Request");
+  } else if (req.url === "/" && req.method === "POST") {
+    //console.log("Request:", req);
+    let body =''
+    req.on('data',(chunk)=>{
+        body+= chunk;
+    });
+    req.on("end", ()=>{
+        const product = JSON.parse(body);
+        console.log("recieved product:", product);
+          res.statusCode = 201;
+          res.end(JSON.stringify({ msg: "product added", product }));
+    });
+  
 
-   else if(req.url==="/about" && req.method==="PUT"){
-      res.statusCode=200;
-      res.end("put request received\n");
-   }
+  } else if (req.url === "/" && req.method === "PUT") {
+    res.statusCode = 200;
+    res.end("PUT Request");
+  } else if (req.url === "/" && req.method === "DELETE") {
+    res.statusCode = 200;
+    res.end("DELETE Request");
+  } else {
+    res.statusCode = 404;
+    res.end("request not found");
+  }
+});
 
-   else if(req.url==="/about" && req.method==="DELETE"){
-      res.statusCode=200;
-      res.end("delete request received\n");
-   }
-
-   else{
-      res.statusCode=404;
-      res.end("Not Found\n");
-   }
-   
+server.listen(5000, () => console.log("prg6 is running"));
